6a7feae910b4c41f9926a154f6add6149a660077
Abstrek <87547358+Abstrek@users.noreply.github.com>
2025-10-26 19:03:24 +0500

    Refactor array initialization and input handling
    Updated array initialization and input loop for clarity.

--- 25-10-2025/array.py
+++ 25-10-2025/array.py
@@ -1,9 +1,9 @@
 #DECLARE myList : ARRAY [0:4] OF STRING 
-myList = ["" for index in range(5)]
-#myList = ["Minhas", "Ali", "Kiran", "Ayaan", "Esha"]
+myList = ["" for index in range(5)] #intializing the array with empty charachters 
+# "" = empty characters
 
-for i in range(len(myList)): 
-    name : str = input("Enter a name: ").upper()
+for i in range(5): 
+    name : str = input("Enter a name: ").upper() #upper function converts all characters into upper-case. 
     myList[i] = name
 
 print(myList)
